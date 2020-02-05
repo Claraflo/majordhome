66db13dd96c281999fce5f2b9fd7cf121e988ac2
NASSURALLY <waseem11@hotmail.fr>
2020-02-05 11:13:18 +0100

    Initialization branch web

---
+++ web/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Accueil</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Lien Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!-- Lien police d'écriture -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+</head>
+<body>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+	<div class="container">
+   		<a class="navbar-brand active" href="index.html" title="">
+        <!-- <img class="logo" src="img/majordhome.png" title="logo" alt="Logo Majord'home"> -->
+       	</a>
+
+    	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        	<span class="navbar-toggler-icon"></span>
+        </button>
+    	
+    	<div class="collapse navbar-collapse" id="navbarResponsive">
+      		<ul class="navbar-nav">
+        		<li class="nav-item active">
+         			<a class="nav-link active" href="index.php" title="">Accueil
+                	<span class="sr-only">(current)</span>
+              		</a>
+        		
+        		
+        		<li class="nav-item">
+         			<a class="nav-link" href="#" title="">A propos</a>
+        		
+        		<li class="nav-item">
+         			<a class="nav-link" href="#" title="">Services</a>
+        		
+          		
+          		<li class="nav-item">
+          			<a class="nav-link" href="#" title="">Contact</a>
+        	</ul>
+    	</div>
+  	</div>
+</nav>
+
+
+
+<section></section>
+
+<footer></footer>
+
+</body>
+</html>
