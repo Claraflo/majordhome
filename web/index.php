777bae3b58e640f8e733abd12c0659e1f87c807f
NASSURALLY <waseem11@hotmail.fr>
2020-02-06 01:14:09 +0100

    Let's go for a beautiful navbar

--- web/index.php
+++ web/index.php
@@ -16,44 +16,54 @@
 </head>
 <body>
 
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+<header>
+<nav class="navbar navbar-expand-lg navbar-dark" id="nav">
 	<div class="container">
-   		<a class="navbar-brand active" href="index.html" title="">
-        <!-- <img class="logo" src="img/majordhome.png" title="logo" alt="Logo Majord'home"> -->
+   		<a class="navbar-brand active" href="index.php" title="">
+        <img class="logo" src="img/majordhome.png" title="logo" alt="Logo Majord'home">
        	</a>
 
-    	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+    	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         	<span class="navbar-toggler-icon"></span>
-        </button>
+      </button>
     	
-    	<div class="collapse navbar-collapse" id="navbarResponsive">
-      		<ul class="navbar-nav">
+    	<div class="collapse navbar-collapse" id="navbarNav">
+      		<ul class="navbar-nav ml-auto">
         		<li class="nav-item active">
-         			<a class="nav-link active" href="index.php" title="">Accueil
+         			<a class="nav-link active colorLink" href="index.php" title="">Accueil
                 	<span class="sr-only">(current)</span>
-              		</a>
+              </a>
         		
+        		
+        		<li class="nav-item ">
+         			<a class="nav-link colorLink" href="#" title="">A propos</a>
         		
         		<li class="nav-item">
-         			<a class="nav-link" href="#" title="">A propos</a>
-        		
-        		<li class="nav-item">
-         			<a class="nav-link" href="#" title="">Services</a>
+         			<a class="nav-link colorLink" href="#" title="">Services</a>
+
+            <li class="nav-item">
+              <a class="nav-link colorLink" href="#" title="">Abonnements</a>
         		
           		
-          		<li class="nav-item">
-          			<a class="nav-link" href="#" title="">Contact</a>
+          	<li class="nav-item">
+          		<a class="nav-link colorLink" href="#" title="">Contact</a>
+
+            <li class="nav-item">
+              
+              <a href="nav-link"><button class="btn" id="btnLogin">Connexion</button></a>
+
         	</ul>
     	</div>
   	</div>
 </nav>
-
-
+</header>
 
 <section></section>
 
 <footer></footer>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
 </html>
