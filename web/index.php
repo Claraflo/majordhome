b074afb52f78978ffc44aa4f4c210b426b45ba82
NASSURALLY <waseem11@hotmail.fr>
2020-02-08 00:33:03 +0100

    Modified footer

--- web/index.php
+++ web/index.php
@@ -330,10 +330,6 @@
   
   <p class="text-center pt-4">Copyright © Majord'home 2020</p>
  
-   
-    </div>
-  </div>
-</div>
 </footer>
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
