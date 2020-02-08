c96d33b50d4b8503e551f694c0359daeb7df5773
Théo <theodoresigaud@gmail.com>
2020-02-08 18:40:40 +0100

    form Register

--- web/pages/register.php
+++ web/pages/register.php
@@ -30,7 +30,60 @@
 </header>
 
 
-<section></section>
+<section class="text-center">
+    <form class="form-signin">
+        <img class="mb-4" src="../img/icon.png" alt="" width="72" height="72">
+
+        <div class="mb-3">
+            <label for="firstName">Prénom</label>
+            <input type="text" id="firstName" class="form-control inputRegister" placeholder="Jean" required="" autocomplete="off" autofocus="">
+        </div>
+
+        <div class="mb-3">
+            <label for="lastName">Nom</label>
+            <input type="text" id="lastName" class="form-control inputRegister" placeholder="Dufour" required="" autocomplete="off">
+        </div>
+
+        <div class="mb-3">
+            <label for="inputEmail">Email</label>
+            <input type="email" id="inputEmail" class="form-control inputRegister" placeholder="jeandufour@gmail.com" required="" autocomplete="off">
+        </div>
+
+        <div class="mb-3">
+            <label for="date">Date de naissance</label>
+            <input type="date" id="date" class="form-control inputRegister" required="" autocomplete="off">
+        </div>
+
+        <div class="mb-3">
+            <label for="phone">Téléphone</label>
+            <input type="tel" id="phone" class="form-control inputRegister" placeholder="0606060606" required="" autocomplete="off">
+        </div>
+
+        <div class="mb-3">
+            <label for="address">Adresse</label>
+            <input type="text" id="address" class="form-control inputRegister" placeholder="Adresse" required="" autocomplete="off">
+        </div>
+
+        <div class="mb-3">
+            <label for="code">Code postal</label>
+            <input type="text" id="code" class="form-control inputRegister" placeholder="75000" required="" autocomplete="off">
+        </div>
+
+        <div class="mb-3">
+            <label for="inputPassword">Mot de passe</label>
+            <input type="password" id="inputPassword" class="form-control inputRegister" required="" autocomplete="off">
+        </div>
+
+        <div class="mb-3">
+            <label for="inputConfirm">Confirmation du mot de passe</label>
+            <input type="password" id="inputConfirm" class="form-control inputRegister" required="" autocomplete="off">
+        </div>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Inscription</button>
+    </form>
+</section>
+
+
 <footer>
 
     <p class="text-center pt-4">Copyright © Majord'home 2020</p>
