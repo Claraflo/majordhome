5f1b350472a6bb56cb8d3402fecdd1a12a66e5bd
NASSURALLY <waseem11@hotmail.fr>
2020-02-09 19:55:03 +0100

    Move 'auth.php' and 'login.php' files on pages folder

--- web/pages/login.php
+++ web/pages/login.php
@@ -8,7 +8,7 @@
 <head>
 	<title>Inscription</title>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="../css/login.css">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
@@ -28,7 +28,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
         <div class="container">
             <a class="navbar-brand active" href="index.php" title="">
-                <img class="logo" src="img/majordhome.png" title="logo" alt="Logo Majord'home">
+                <img class="logo" src="../img/majordhome.png" title="logo" alt="Logo Majord'home">
             </a>
         </div>
     </nav>
