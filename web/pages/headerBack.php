12299626440786450d844479d8f9e16a4e6c0e13
NASSURALLY <waseem11@hotmail.fr>
2020-02-16 22:22:23 +0100

    Header and footer backoffice

---
+++ web/pages/headerBack.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BackOffice</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="../css/headerBack.css">
+	<link rel="stylesheet" type="text/css" href="../css/category.css">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Lien Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<!-- Lien police d'écriture -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+
+  <!-- Lien Icon -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+</head>
+<body>
+
+
+<header>
+	<div class="userConnected">
+		
+		<p>Connecté en mode : Employé</p>
+
+	</div>
+    <nav class="navbar navbar-expand-lg navbar-dark" id="nav">
+        <div class="container-fluid">
+            <a class="navbar-brand active" href="index.php" title="">
+                <img class="logo" src="../img/majordhome.png" title="logo" alt="Logo Majord'home">
+            </a>
+
+
+    	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        	<span class="navbar-toggler-icon"></span>
+      </button>
+    	
+    	<div class="collapse navbar-collapse" id="navbarNav">
+      		<ul class="navbar-nav ml-auto">
+
+
+        <li class="nav-item dropdown">
+        <a class="nav-link colorLink dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Gestion
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Utilisateurs</a>
+          <a class="dropdown-item" href="#">Catégories</a>
+          <a class="dropdown-item" href="#">Abonnements</a>
+          <a class="dropdown-item" href="#">Services</a>
+          <a class="dropdown-item" href="#">Réservations</a>
+          <a class="dropdown-item" href="#"></a>
+        </div>
+      
+        		
+        
+
+            <li class="nav-item">
+              <a class="nav-link colorLink" href="#" title="">Boîte de réception</a>
+
+
+
+            <li class="nav-item">
+              <a class="nav-link colorLink" href="#" title="">Devis</a>
+
+
+            <li class="nav-item">
+              <a class="nav-link colorLink" href="#" title="">Factures</a>
+        		
+          		
+          	<li class="nav-item">
+
+          		 <a class="nav-link colorLink" href="#" title="">Mon compte</a>
+
+
+          		<li class="nav-item">
+
+          		  <a href="logout.php"><button class="btn" id="btnLogout">Déconnexion</button></a> 
+          		
+
+        	</ul>
+    	</div>
+        </div>
+    </nav>
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
