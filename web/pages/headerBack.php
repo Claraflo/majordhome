2dd65cf48ad179c385f7222e8dbbfd04f72c69f1
NASSURALLY <waseem11@hotmail.fr>
2020-02-16 22:35:41 +0100

    Modified header back

--- web/pages/headerBack.php
+++ web/pages/headerBack.php
@@ -1,26 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>BackOffice</title>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="../css/headerBack.css">
-	<link rel="stylesheet" type="text/css" href="../css/category.css">
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-	<!-- Lien Bootstrap -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-	<!-- Lien police d'écriture -->
-	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-
-  <!-- Lien Icon -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-
-</head>
-<body>
-
-
 <header>
 	<div class="userConnected">
 		
