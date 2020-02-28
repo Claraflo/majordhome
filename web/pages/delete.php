42d3c799018e3dfc925887f1a3e94937f4141ee0
Théo <theodoresigaud@gmail.com>
2020-02-28 17:56:59 +0100

    delete

---
+++ web/pages/delete.php
@@ -0,0 +1,27 @@
+<?php
+session_start ();
+
+if(!empty($_POST['id'])) {
+
+    $id = $_POST['id'];
+
+
+
+    try{
+        $bdd = new PDO("mysql:host=localhost;dbname=subscription;port=3306", "root", "root");
+    }catch(Exception $e){
+        die("Erreur SQL ".$e->getMessage());
+    }
+
+
+    $req = $bdd->prepare("UPDATE customer set status =:status WHERE id =:id;");
+    $req->execute([
+        ':status'=>-1,
+        ':id'=>$id
+    ]);
+
+}
+
+
+
+?>
