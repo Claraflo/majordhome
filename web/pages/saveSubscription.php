f0656e4c24d7bd44fd471e33de1803b712a3b3dc
Théo <theodoresigaud@gmail.com>
2020-02-28 17:47:02 +0100

    Modification

--- web/pages/saveSubscription.php
+++ web/pages/saveSubscription.php
@@ -102,14 +102,14 @@ if(count($_POST) == 11
         $req->execute([':title'=>$title,
             ':price'=>$price,
             ':description'=>$description,
-            'years'=>$years,
-            'months'=>$months,
-            'days'=>$days,
-            'week'=>$week,
-            'time'=>$time,
-            'timeStart'=>$timeStart,
-            'timeEnd'=>$timeEnd,
-            'status'=>0
+            ':years'=>$years,
+            ':months'=>$months,
+            ':days'=>$days,
+            ':week'=>$week,
+            ':time'=>$time,
+            ':timeStart'=>$timeStart,
+            ':timeEnd'=>$timeEnd,
+            ':status'=>0
         ]);
 
 
