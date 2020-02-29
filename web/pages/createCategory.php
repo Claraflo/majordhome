a846b950c7b757aeffbfb9297b5738ebf61324a9
NASSURALLY <waseem11@hotmail.fr>
2020-02-29 14:22:03 +0100

    Fix bug verify category

--- web/pages/createCategory.php
+++ web/pages/createCategory.php
@@ -1,18 +1,43 @@
 <?php
 require('functions.php');
-if (isset($_POST['name']) && !empty($_POST['name'])) {
+if (!empty($_POST['name'])) {
 
 
 	$name = trim($_POST['name']);
-	
+	$description = trim($_POST['description']);
 
-	if (isset($_POST['description']) && $name != "") {
+	$errors = [];
+
+    		
+    if(strlen($description)>150){
+    				
+    	$errors[]= "Votre description doit être inférieur à 150 caractères !";
+   	}
 
 
-		$connect = connectDb();
+	$connect = connectDb();
 
-		$description = trim($_POST['description']);
-		
+
+   	$queryPrepared = $connect->prepare("SELECT nom FROM Categorie WHERE nom = :name");
+   	$queryPrepared->execute([":name" => $name]);
+   	$data = $queryPrepared->fetch(PDO::FETCH_ASSOC);
+
+
+   	if (!empty($data)) {
+
+   		$errors[]= "La catégorie existe déjà ";
+   		
+   	}else{
+
+   		if(strlen($name)<2 || strlen($name)>80){
+    			
+    		$errors[]= "nom trop court ou trop long ! ";
+   		}
+   	}
+
+   	if (empty($errors)) {
+   		$connect = connectDb();
+			
 		$queryPrepared = $connect->prepare("INSERT INTO Categorie(nom,description) VALUES(:name,:description);");
 		$queryPrepared->execute([
 			":name"=>$name,
@@ -20,13 +45,24 @@ if (isset($_POST['name']) && !empty($_POST['name'])) {
 		]);
 
 
-		echo "<div class='alert alert-success'>Catégorie créée ! </div>";
+		echo "<div class='alert alert-success'> Catégorie créée !</div>";
 
-	}else{
 
-		echo "<div class='alert alert-danger'>Erreur de saisie !</div>";
+   	}else{
 
-	}
+
+   			echo "<div class='alert alert-danger'>";
+   			foreach ($errors as  $value) {
+				echo "<li>".	$value. "</li>";
+	
+   			}
+
+   			echo "</div>";
+   	}
+
+
+	
+	
 
 
 }else{
