3064e58d1305cc826e9f3caf86a7e42fc4f9796d
Théo <theodoresigaud@gmail.com>
2020-03-29 17:37:07 +0200

    Add modification service

--- web/18_03_2020/pages/back/modificationReservationBack.php
+++ web/18_03_2020/pages/back/modificationReservationBack.php
@@ -13,7 +13,7 @@ if(!empty($_GET)){
     $id = 0;
 }
 
-$data = $connect->prepare("SELECT idSouscriptionService, FK_idService FROM souscription_service WHERE idSouscriptionService = '".$id."'");
+$data = $connect->prepare("SELECT idSouscriptionService, FK_idService, duree, dateIntervention FROM souscription_service WHERE idSouscriptionService = '".$id."'");
 $data->execute(array());
 
 $count = $data->rowCount();
@@ -27,26 +27,29 @@ if ($count == 0){
 $req = $connect->prepare("SELECT idCaracteristique, nom, type FROM caracteristique WHERE idService = ".$souscription['FK_idService']);
 $req->execute(array());
 
-$result = $connect->prepare("SELECT idSouscriptionService, FK_idService FROM donnees_service WHERE FK_idSouscriptionService = '".$id."'");
-$result->execute(array());
-$value = $result->fetch();
+
 
 $idCaracteristique = [];
+
+
 ?>
 
 <section >
     <form method="post" action="saveServiceBack.php">
         <div class="container borderSubscription">
             <label for="intervention" class="lab area">Date de l'intervention</label>
-            <input name="intervention" type="date"id="intervention" class="form-control inputRegister" required="">
+            <input name="intervention" type="date" id="intervention" class="form-control inputRegister" value="<?php echo $souscription['dateIntervention']?>" required="">
 
             <label for="time" class="lab area">Durée</label>
-            <input name="time" type="time"id="time" class="form-control inputRegister" required="">
+            <input name="time" type="time" id="time" class="form-control inputRegister" value="<?php echo $souscription['duree']?>" required="">
             <?php
             foreach ($req->fetchAll() as $caracteristique) {
                 $idCaracteristique[] = $caracteristique['idCaracteristique'];
+                $result = $connect->prepare("SELECT information FROM donnees_service WHERE FK_idSouscriptionService = '".$id."' AND FK_idCaracteristique = ".$caracteristique['idCaracteristique']);
+                $result->execute(array());
+                $value = $result->fetch();
                 echo '<label for="'.$caracteristique["nom"].'" class="lab area">'.$caracteristique["nom"].'</label>';
-                echo '<input name="'.$caracteristique["nom"].'" type="'.$caracteristique["type"].'" id="'.$caracteristique["nom"].'" class="form-control inputRegister" required="">';
+                echo '<input name="'.$caracteristique["nom"].'" type="'.$caracteristique["type"].'" id="'.$caracteristique["nom"].'" class="form-control inputRegister" value="'.$value['information'].'" required="">';
             }
             $_SESSION['idCaracteristique'] = $idCaracteristique;
             ?>
@@ -60,7 +63,7 @@ $idCaracteristique = [];
                 </select>
             </div>
 
-            <input type="submit" class="btn btn-success area" value="Valider l'enregistrement">
+            <input type="submit" class="btn btn-success area" value="Valider la modification">
 
         </div>
     </form>
