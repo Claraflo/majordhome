cdd0dc208b4c718e3d869057fa201c0539ed7fea
NASSURALLY <waseem11@hotmail.fr>
2020-03-30 14:41:06 +0200

    initialization account

--- web/18_03_2020/pages/front/header.php
+++ web/18_03_2020/pages/front/header.php
@@ -60,7 +60,7 @@ if (!isset($_SESSION['user']) || $_SESSION['user']['statut'] != 0) {
 
                     <li class="nav-item">
 
-                        <a class="nav-link colorLink" href="#" title="">Mon compte</a>
+                        <a class="nav-link colorLink" href="myAccount.php" title="">Mon compte</a>
 
 
                     <li class="nav-item">
