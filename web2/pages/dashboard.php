454876d50be987567352ffb9050b45b69aa535e5
Théo <theodoresigaud@gmail.com>
2020-04-11 16:33:03 +0200

    Calendar Initialization

--- web2/pages/dashboard.php
+++ web2/pages/dashboard.php
@@ -1,3 +1,61 @@
 <?php
 require("header.php");
+
+$mois = date('m');
+$annee = date('Y');
 ?>
+
+<body>
+
+<div id="arrow">
+    <i class="fas fa-arrow-circle-left fa-3x" id="prev"></i>
+    <i class="fas fa-arrow-circle-right fa-3x" id="next"></i>
+</div>
+
+<div id="content">
+</div>
+
+<script type="text/javascript">
+
+    jQuery(function($){
+
+        var mois = <?php echo $mois; ?>;
+        var annee = <?php echo $annee; ?>;
+
+        $(document).ready(function(){
+
+            $("#content").load("calendar.php?mois="+mois+"&annee="+annee+"");
+
+        });
+
+        $("#prev").click(function(){
+
+            mois--;
+
+            if (mois < 1) {
+                annee--;
+                mois = 12;
+            }
+
+            $("#content").load("calendar.php?mois="+mois+"&annee="+annee+"");
+
+        });
+
+        $("#next").click(function(){
+
+            mois++;
+
+            if (mois > 12) {
+                annee++;
+                mois = 1;
+            }
+
+            $("#content").load("calendar.php?mois="+mois+"&annee="+annee+"");
+
+        });
+
+    });
+
+</script>
+</body>
+</html>
