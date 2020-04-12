ed6d15cc31ee0870a8a03f77afc1e214c21e2321
Théo <theodoresigaud@gmail.com>
2020-04-12 14:30:28 +0200

    Double tab

--- web2/pages/attribution.php
+++ web2/pages/attribution.php
@@ -11,125 +11,83 @@ $date = explode('-', $date);
 
 ?>
 
-<body>
+<body id="attribution">
 
 <h1 id="titleAttribution"><?php echo $date[2]."-".$date[1]."-".$date[0] ?></h1>
 
 
 
-<!--    <table class="table table-borderless" id="tableAttribution">-->
-<!--        <thead>-->
-<!--        <tr>-->
-<!--            <th scope="col">Clients</th>-->
-<!--            <th scope="col">Last</th>-->
-<!--            <th scope="col">Handle</th>-->
-<!--            <th scope="col">Prestataires</th>-->
-<!---->
-<!--        </tr>-->
-<!--        </thead>-->
-<!--        <tbody>-->
-<!--        <tr>-->
-<!--            <td>Mark</td>-->
-<!--            <td></td>-->
-<!--            <td></td>-->
-<!--            <td>Mark</td>-->
-<!--        </tr>-->
-<!--        <tr>-->
-<!--            <td>Jacob</td>-->
-<!--            <td></td>-->
-<!--            <td></td>-->
-<!--            <td>Mark</td>-->
-<!--        </tr>-->
-<!--        <tr>-->
-<!--            <td>Larry</td>-->
-<!--            <td></td>-->
-<!--            <td></td>-->
-<!--            <td>Mark</td>-->
-<!--        </tr>-->
-<!--        </tbody>-->
-<!--    </table>-->
-<!--<style>-->
-<!--    TABLE {-->
-<!--        float : left;-->
-<!--        margin-left : 5px;-->
-<!--    }-->
-<!--</style>-->
-<!---->
-<!--<table border="1">-->
-<!--    <tr>-->
-<!--        <td>fef </td>-->
-<!--        <td>effe </td>-->
-<!--    </tr>-->
-<!--    <tr>-->
-<!--        <td>fefe </td>-->
-<!--        <td>feef </td>-->
-<!--    </tr>-->
-<!--</table>-->
-<!--<table border="1">-->
-<!--    <tr>-->
-<!--        <td>effe </td>-->
-<!--        <td>effe </td>-->
-<!--    </tr>-->
-<!--    <tr>-->
-<!--        <td>efe </td>-->
-<!--        <td>efe </td>-->
-<!--    </tr>-->
-<!--</table>-->
-<!--<br clear="left">-->
-<!--<table border="1">-->
-<!--    <tr>-->
-<!--        <td> rffr</td>-->
-<!--        <td>frf </td>-->
-<!--    </tr>-->
-<!--    <tr>-->
-<!--        <td>frfr </td>-->
-<!--        <td>fr </td>-->
-<!--    </tr>-->
-<!--</table>-->
-<!---->
-<!--</body>-->
-
 <div class="row">
     <div class="table-responsive col-md-6">
-            <table class="table table-borderless" id="tableAttribution">
-                <thead>
-                <tr>
-                    <th scope="col">Clients</th>
-                    <th scope="col">Last</th>
+            <div class="overflow">
+                <table class="table table-borderless" id="tableAttribution">
+                    <thead>
+                    <tr>
+                        <th scope="col">Clients</th>
+                        <th scope="col">Last</th>
 
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Mark</td>
-                    <td>Mark</td>
-                </tr>
-                <tr>
-                    <td>Jacob</td>
-                    <td>Mark</td>
-                </tr>
-                </tbody>
-            </table>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
     </div>
     <div class="table-responsive col-md-6">
-            <table class="table table-borderless" id="tableAttribution">
-                <thead>
-                <tr>
-                    <th scope="col">Clients</th>
-                    <th scope="col">Last</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>Mark</td>
-                    <td>Mark</td>
-                </tr>
-                <tr>
-                    <td>Jacob</td>
-                    <td>Mark</td>
-                </tr>
-                </tbody>
-            </table>
+            <div class="overflow">
+                <table class="table table-borderless" id="tableAttribution">
+                    <thead>
+                    <tr>
+                        <th scope="col">Prestataire</th>
+                        <th scope="col">Last</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>Mark</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    <tr>
+                        <td>Jacob</td>
+                        <td>Mark</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
     </div>
 </div>
+
+</body>
 </html
