186beef3f5b8ca60e3e5d22c0cf2a00117b368c0
Théo <theodoresigaud@gmail.com>
2020-04-12 13:17:35 +0200

    Initialization atttribution

--- web2/pages/attribution.php
+++ web2/pages/attribution.php
@@ -1,1 +1,32 @@
 <?php
+require("header.php");
+
+if (!isset($_GET['date'])){
+    header('Location: dashboard.php');
+}
+
+$date = $_GET['date'];
+
+$date = explode('-', $date);
+
+?>
+<h1 id="titleAttribution"><?php echo $date[2]."-".$date[1]."-".$date[0] ?></h1>
+
+<table class="table table-striped" id="tabAttribution">
+    <thead>
+    <tr>
+        <th scope="col">Prestataires</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row">1</th>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+    </tr>
+    </tbody>
+</table>
