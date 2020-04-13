764e701b566c33c1bcf3b9875f2b230a0152c1a2
Théo <theodoresigaud@gmail.com>
2020-04-13 12:28:39 +0200

    Add save and delete

--- web2/pages/deleteAttribution.php
+++ web2/pages/deleteAttribution.php
@@ -4,12 +4,11 @@ require('functions.php');
 $connect = connectDb();
 
 $idCustomer = $_POST['idCustomer'];
-$idProvider = $_POST['idProvider'];
 $idSouscriptionService = $_POST['idSouscriptionService'];
 
 
 $req = $connect->prepare("UPDATE souscription_service set FK_idPrestataire =:FK_idPrestataire WHERE idSouscriptionService =:idSouscriptionService AND FK_idPersonne =:FK_idPersonne;");
-$req->execute([':FK_idPrestataire' => $idProvider,
+$req->execute([':FK_idPrestataire' => NULL,
     ':idSouscriptionService'=> $idSouscriptionService,
     ':FK_idPersonne' => $idCustomer
 ]);
